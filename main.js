c981ff66c0db70b3b3e83bd537a988b841878b84
Ayanprakash351 <74969381+Ayanprakash351@users.noreply.github.com>
2021-07-29 14:59:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+mousX = 0;
+mousY = 0;
+function preload(){
+
+}
+function setup()
+{
+   canvas = createCanvas(300, 300);
+   canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+  save('gentlemen.png')
+}
